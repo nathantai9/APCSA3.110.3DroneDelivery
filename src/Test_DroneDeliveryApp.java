5c9a85f08cc7f4022b2152b7f9796912c48bb1a5
Nathan Tai <mregamer416@gmail.com>
2026-01-21 02:12:57 +0000

    I meant commenting out the unused imports, not deleting them entirely.

--- src/Test_DroneDeliveryApp.java
+++ src/Test_DroneDeliveryApp.java
@@ -1,6 +1,6 @@
 import static org.junit.Assert.assertEquals;
-// import static org.junit.Assert.assertTrue;
-// import static org.junit.Assert.assertFalse;
+// import static org.junit.Assert.assertTrue; commented out unused import
+// import static org.junit.Assert.assertFalse; commented out unused import
 import org.junit.Test;
 
 public class Test_DroneDeliveryApp {
