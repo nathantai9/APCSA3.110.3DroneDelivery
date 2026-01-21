5ba893c9859d8159236cca132d9a1e30967354b0
Nathan Tai <mregamer416@gmail.com>
2026-01-21 02:11:58 +0000

    fixed Test_DroneDeliveryApp.java to get rid of unused imports.

--- src/Test_DroneDeliveryApp.java
+++ src/Test_DroneDeliveryApp.java
@@ -1,6 +1,6 @@
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
+// import static org.junit.Assert.assertTrue;
+// import static org.junit.Assert.assertFalse;
 import org.junit.Test;
 
 public class Test_DroneDeliveryApp {
